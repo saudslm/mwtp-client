983122b1278d2df571d4388aa62d1f5839ee9467
Saud <saudslm@gmail.com>
2018-02-25 11:21:49 +0800

    routing

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const app = express();
 
 app.use( express.static(__dirname + "/dist") );
 app.listen(process.env.PORT || 8080);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/dist/index.html');
+});
+app.get('/login', function(req, res){
+    res.sendFile(__dirname + '/dist/index.html');
+});
