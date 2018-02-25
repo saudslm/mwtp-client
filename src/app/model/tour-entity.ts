38dd09e81dfdf54823b11dfd6f80a46a100cc6f6
Saud <saudslm@gmail.com>
2018-02-25 11:00:13 +0800

    routing

---
+++ src/app/model/tour-entity.ts
@@ -0,0 +1,15 @@
+export class TourEntity {
+    public id: number;
+    public title: string;
+    public purpose: string;
+    public startDate: string;
+    public endDate: string;
+    public endDate: string;
+    public modeOfTravel: string;
+    public ticketCost: number;
+    public airportCabHomeCityCost: number;
+    public airportCabDestinationCityCost: number;
+    public hotelCost: number;
+    public otherConveyanceCost: number;
+    public status: string;
+}
